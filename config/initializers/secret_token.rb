a0a3bd612e11ae3efbe78e8cd86a64860155f188
Tracy Weiss <tracyleeweiss@gmail.com>
2013-04-21 18:44:36 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FriendslibraryApp::Application.config.secret_token = '077ac88e3718d4be7eb916341ffb23eb2f21af3338d90c247292d29311d199325f802eafd649983940f6976a525632e0002511b67e2981f21f04c0c2d99c984a'
