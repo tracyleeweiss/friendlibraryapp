f61b2ccca3c5da4a467e8b86b947bdae5a0b3205
Tracy Weiss <tracyleeweiss@gmail.com>
2013-05-05 10:39:44 -0700

    added title helper and stylesheets

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,23 +3,51 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    @base_title = "Lend to Friends App"
+  end
+
   describe "GET 'home'" do
-    it "returns http success" do
+    it "should be succcessful" do
       get 'home'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                                    :content => "#{@base_title} | Home")
+    end
+
+    it "should have a non-blank body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\?body>/
+    end
 end
+
   describe "GET 'contact'" do
-    it "returns http success" do
+    it "should be successful" do
       get 'contact'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                                    :content => "#{@base_title} | Contact")
   end
+end
 
 describe "GET 'about'" do
-    it "returns http success" do
+    it "should be successful" do
       get 'about'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                                     :content => "#{@base_title} | About")
     end
   end
 
